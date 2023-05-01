e627dad9dffd153fb259b4c22d11df9928daf6d3
athar0x1 <74399841+athar0x1@users.noreply.github.com>
2023-05-01 11:36:11 +0530

    Update rezo.py

--- rezo.py
+++ rezo.py
@@ -1,4 +1,4 @@
-rezo.ai
-apikey="3437836yfbgehgcerivehcv"
-token="2387643782dghcvehc"
-password="lololololololo"
+
+apikey=">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
+token="<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<"
+password="edefefefefefe"
